37674ea47650566ab409a41f3d51479ba16917d2
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-24 01:14:32 +0300

    Add Location, Category, and Post models with relationships

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
